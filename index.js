21bda08104b7b23cde3afe24517c780891786aca
liupeng <liupeng@ztgame.com>
2020-08-20 10:31:35 +0800

    发布代码

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 #!/usr/bin/env node
-const program = require("commander");
+const commander = require("commander");
 const fs = require("fs-extra");
-const configFile = "./.focusbe";
-class Focusbe{
-    async static getConfig(){
-       var res = await fs.exists(configFile);
-       if(!res){
-           return null;
-       }
-       var config = await fs.readJson(configFile);
-       return config;
-    }
-    static create(){
-        //创建一个全新的项目
-    }
-    async static init(config){
-        //初始化已有项目
-        return await fs.writeJson(configFile,config);
-    }
-    static publish(){
-        //把
-    }
-}
-program.version("1.0.0", "-v, --version").command("init <name>");
+const DevTools = require("@ztgame/devtools");
+var devTools = new DevTools();
+commander
+  .version("1.0.0", "-v, --version")
+  .command("init")
+  .action(() => {
+    devTools.init();
+  });
+commander.command("pubdev").action(() => {
+  devTools.pubdev();
+});
+commander.parse(process.argv);
