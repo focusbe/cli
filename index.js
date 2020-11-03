d8a2616678da9fc1753d01ef9ca513c22dfc709a
liupeng <liupeng@ztgame.com>
2020-11-03 19:00:27 +0800

    添加构建代码功能

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 const commander = require("commander");
-const fs = require("fs-extra");
+
 const DevTools = require("@ztgame/devtools");
 var devTools = new DevTools();
+const Build = require("./build");
 commander
   .version("1.0.0", "-v, --version")
   .command("init")
   .action(() => {
     devTools.init();
   });
+commander.command("dev").action(() => {
+  
+});
 commander.command("pubdev").action(() => {
   devTools.pubdev();
 });
