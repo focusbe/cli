0508cea557622b5cda8f80e81b4ae839903eb419
liupeng <liupeng@ztgame.com>
2020-11-05 11:52:03 +0800

    修改ts 为js

--- build/index.js
+++ build/index.js
@@ -1,24 +1,29 @@
 const rollup = require("rollup");
 const path = require("path");
-const typescript = require("@rollup/plugin-typescript");
-// const babel = require("@rollup/plugin-babel").babel;
+const babel = require("@rollup/plugin-babel");
+const getBabelOutputPlugin = babel.getBabelOutputPlugin;
+
 const fse = require("fs-extra");
 const stylus = require("stylus");
 const poststylus = require("poststylus");
 const browserSync = require("browser-sync");
 const chokidar = require("chokidar");
 class Build {
-  constructor(basePath) {
+  constructor(basePath, isDev = true) {
+    this.isDev = isDev;
     this.basePath = basePath;
-    this.inputJs = path.resolve(this.basePath, "./scripts/main.ts");
-    this.outputJs = this.inputJs.replace(".ts", ".js");
+    this.inputJs = path.resolve(this.basePath, "./scripts/main.js");
+    this.outputJs = this.inputJs.replace("main.js", "bundle.js");
     this.inputCss = path.resolve(this.basePath, "./css/main.styl");
     this.outputCss = this.inputCss.replace(".styl", ".css");
   }
   start() {
     //
     //
-    this.server();
+    if (this.isDev) {
+      this.server();
+    }
+
     this.buildcss();
     this.buildjs();
   }
@@ -40,8 +45,6 @@ class Build {
         ignoreInitial: true,
       })
       .on("all", (event, file) => {
-        // console.log(event,path);
-        // console.log(event, path);
         if (path.resolve(this.basePath, file) == this.outputCss) {
           return;
         }
@@ -56,7 +59,7 @@ class Build {
         .render((err, css) => {
           if (err) throw err;
           fse.writeFile(this.outputCss, css, (err) => {
-            if (!err) {
+            if (!err && this.bs) {
               this.bs.reload("./css/main.css");
             }
           });
@@ -70,12 +73,39 @@ class Build {
       };
       const watcher = rollup.watch({
         input: this.inputJs,
-        plugins: [typescript()],
+        plugins: [
+          babel.babel({
+            exclude: /(node_modules|babel|corejs|core-js)/, //千万不要babel babel的代码
+            babelHelpers: "bundled",
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: "last 3 version ,ie >= 8",
+                  },
+                },
+              ],
+            ],
+            // plugins: [
+            //   [
+            //     "@babel/plugin-transform-runtime",
+            //     {
+            //       corejs: 3,
+            //       regenerator: true,
+            //     },
+            //   ],
+            // ],
+          }),
+        ],
         output: outputOptions,
       });
       watcher.on("event", (event) => {
-        if (event.code == "END") {
-          this.bs.reload("./js/main.js");
+        if (event.code.indexOf("ERROR") > -1) {
+          console.log(event);
+        }
+        if (event.code == "END" && this.bs) {
+          this.bs.reload("./js/bundle.js");
         }
       });
       return true;
