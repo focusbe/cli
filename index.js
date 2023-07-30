754d26571db85f08939cd394cd85af03e715fb33
pengzai <roc.liu@hotmail.com>
2023-07-30 14:10:26 +0800

    优化

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 #!/usr/bin/env node
-const commander = require("commander");
-const DevTools = require("@ztgame/devtools");
-var devTools = new DevTools();
-const Build = require("./build");
-//
+const commander = require('commander');
+const fs = require('fs');
+const Build = require('./build');
 var projectRoot = process.cwd();
 var build = new Build(projectRoot);
 commander
-  .version("1.0.0", "-v, --version")
-  .command("init")
-  .action(() => {
-    devTools.init();
-  });
-commander.command("dev").action(() => {
-  build.start();
-});
-commander.command("pubdev").action(() => {
-  devTools.pubdev();
+    .version('1.0.0', '-v, --version')
+    .command('init')
+    .action(() => {
+        //初始化项目
+        // 将模板文件拷贝到项目中
+        // 监测是否有package.json文件
+        if (fs.existsSync(projectRoot + '/package.json')) {
+            console.warn('请在空项目中初始化');
+            return;
+        }
+        fs.copyDirSync(__dirname + '/template', projectRoot);
+        // 将依赖安装到项目中
+    });
+commander.command('dev').action(() => {
+    build.start();
 });
 
 commander.parse(process.argv);
